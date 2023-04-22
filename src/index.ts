0209b6579388e950aa2915cb0f0562bbd89c73b4
Aleksey P <aleksplus@users.noreply.github.com>
2023-04-22 15:54:31 +0300

    fix: export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { MyShows } from './components/base';
 import { MyShowsCore } from './components/core';
 
+export * from './enums';
+export * from './types/index';
+
 export { MyShows, MyShowsCore };
